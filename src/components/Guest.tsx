53adb00e53b5ba88042c00a056d64af0a6699a3e
Alican Şan <alicansan2606@gmail.com>
2024-08-12 17:04:04 +0300

    updates

--- src/components/Guest.tsx
+++ src/components/Guest.tsx
@@ -5,7 +5,7 @@ const Guest = () => {
     <div className="flex flex-col items-center text-center">
       <h1>Welcome</h1>
       <p>Please sign in to manage your transactions</p>
-      <SignIn />
+      <SignIn routing="hash" />
     </div>
   );
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -10,3 +10,17 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
+
+//THE ERROR
+
+// Error:
+// Clerk: The <SignIn/> component is not configured correctly. The most likely reasons for this error are:
+
+// 1. The "/" route is not a catch-all route.
+// It is recommended to convert this route to a catch-all route, eg: "//[[...rest]]/page.tsx". Alternatively, you can update the <SignIn/>
+// component to use hash-based routing by setting the "routing" prop to "hash".
+
+// 2. The <SignIn/> component is mounted in a catch-all route, but all routes under "/" are protected by the middleware.
+// To resolve this, ensure that the middleware does not protect the catch-all route or any of its children. If you are
+//  using the "createRouteMatcher" helper, consider adding "(.*)" to the end of the route pattern, eg: "/(.*)".
+//  For more information, see: https://clerk.com/docs/references/nextjs/clerk-middleware#create-route-matcher
